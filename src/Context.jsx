694bcac1c68acb4fdd3315e3d87607e1b8089403
Viktor Filippov <jackal3166@gmail.com>
2023-10-29 11:08:40 +0200

    Add localStorage

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,19 +1,28 @@
-import { createContext, useContext } from 'react';
-
-import { useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+	const prefersDarkMode = window.matchMedia(
+		'(prefers-color-scheme: dark)'
+	).matches;
+    const storedDarkMode = localStorage.getItem('darkTheme') === 'true';
+	return storedDarkMode || prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-	const [isDarkTheme, setIsDarkTheme] = useState(false);
+	const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
 	const [searchTerm, setSearchTerm] = useState('Nintendo');
 
 	const toggleDarkTheme = () => {
 		const newDarkTheme = !isDarkTheme;
 		setIsDarkTheme(newDarkTheme);
-		const body = document.querySelector('body');
-		body.classList.toggle('dark-theme', newDarkTheme);
+        localStorage.setItem('darkTheme', newDarkTheme);
 	};
+
+	useEffect(() => {
+		document.body.classList.toggle('dark-theme', isDarkTheme);
+	}, [isDarkTheme]);
 
 	return (
 		<AppContext.Provider
